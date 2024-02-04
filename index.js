9071540eda57219ba39f8c886137a4248910e78a
luis_jh <160544@unsaac.edu.pe>
2024-02-03 19:07:35 -0500

    test 4

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import fs from "fs";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
+
 app.use(bodyParser.json());
 
 const readData = () => {
